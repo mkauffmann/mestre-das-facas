c8e6ece26d56555354569041dd93846e71cb22d5
Maria Kauffmann <mariatkauffmann@gmail.com>
2021-09-13 21:07:40 -0300

    menu mobile e desktop da dashboard

---
+++ js/dashboard.js
@@ -0,0 +1,10 @@
+//MENU MOBILE INICIO
+let btnArrow = document.querySelector('.btn-arrow');
+let itensMenu = document.querySelector('.menu-mobile-items-container');
+
+
+btnArrow.addEventListener('click', () => {
+itensMenu.classList.toggle('menu-fechado')
+});
+
+//MENU MOBILE FIM
